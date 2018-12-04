d81dc32cdd4ac286bc43002dda5d7547334c09ba
1409770 <1409770@LLBD0603INFO19>
2018-12-03 19:01:05 -0500

    angjson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MaterialModule} from './material/material.module';
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {LoginComponent} from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    MaterialModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent }
+    ]),
     BrowserModule
   ],
   providers: [],

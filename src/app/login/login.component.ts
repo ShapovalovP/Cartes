fbcd8aaa60384ac95e4e9894481f2354b05c5aa7
1409770 <1409770@LLBD0603INFO19>
2018-12-03 20:45:18 -0500

    Create login.component.ts

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {AppComponent} from '../app.component';
+import {Login} from './login';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  public loginUser: Login = new Login( '' , '');
+  constructor(public http: HttpClient, public router: Router, public app: AppComponent) { }
+  public User: string = localStorage.getItem('User') ;
+  ngOnInit() {
+  }
+  validerLog(): boolean {
+    if (this.loginUser.username !== ''  ) {//&& this.loginUser.password !== ''
+      return true;
+    }
+    return false;
+  }
+  validerPw(): boolean {
+    if (this.loginUser.password !== ''  ) {//&& this.loginUser.password !== ''
+      return true;
+    }
+    return false;
+  }
+  login() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+      })
+    };
+
+    const body = new HttpParams()
+      .set('username', this.loginUser.username)
+      .set('password', this.loginUser.password)
+      .set('grant_type', this.loginUser.grant_type);
+
+    this.http.post<any>('/api/Token', body.toString(), httpOptions)
+      .subscribe(response => {
+        localStorage.setItem('Token', response.access_token);
+        localStorage.setItem('User', response.userName);
+        this.app.User = localStorage.getItem('User') ;
+        this.router.navigate(['/']);
+
+      });
+  }
+}

e3dc0ab9aef34767eab1a084c834dd7b3517e392
Philippe Morin <43551181+Kademor@users.noreply.github.com>
2018-12-07 14:58:28 -0500

    rendre beau le deck creator, add carte to deck still doesnt work

--- src/app/deck-creator/deck-creator.component.ts
+++ src/app/deck-creator/deck-creator.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {Carte} from '../carte/carte';
 
 @Component({
   selector: 'app-deck-creator',
@@ -8,9 +10,20 @@ import {Router} from '@angular/router';
 })
 export class DeckCreatorComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  public carte: Carte[] = [];
+  constructor(public router: Router, public http: HttpClient) { }
+
 
   ngOnInit() {
+
+    this.http.get<any>('api/Cartes'). subscribe( response => {
+      console.log(response);
+      for (const carte of response) {
+        this.carte.push(new Carte(carte.Id, carte.ValeurAttaque,
+          carte.ValeurDefense, carte.prixAchat, carte.prixVendre,
+          carte.image, carte.imageDerier, 0));
+      }
+    });
   }
 
 }

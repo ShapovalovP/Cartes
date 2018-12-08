09b08fbbecbb0661e2764a804ea2d27224a1d64c
Cédric Fiset <cedfis46@gmail.com>
2018-12-08 18:36:48 -0500

    marche achat vendre

--- src/app/marche/marche.component.ts
+++ src/app/marche/marche.component.ts
@@ -9,11 +9,14 @@ import {Carte} from '../carte/carte';
   styleUrls: ['./marche.component.css']
 })
 export class MarcheComponent implements OnInit {
-  public carte: Carte[] = [];
+  public carte: CarteUser[] = [];
+  public carteUser: CarteUser[] = [];
 
   constructor(public router: Router, public http: HttpClient) { }
 
-  ngOnInit() {
+  public achete(cu:CarteUser){
+    const c=new Carte(cu.id,cu.valeurAttaque,cu.valeurDefense,cu.prixAchat,cu.prixVendre,cu.image,cu.imageDerier,cu.rezBatail);
+    const body = c;
     const token = localStorage.getItem('Token');
     const httpOptions = {
       headers: new HttpHeaders({
@@ -21,12 +24,70 @@ export class MarcheComponent implements OnInit {
         'Authorization': 'Bearer ' + token
       })
     };
-    this.http.get<any>('api/Cartes', httpOptions ). subscribe( response => {
-      console.log(response);
-      for (const carte of response) {
-          this.carte.push(new Carte(carte.Id, carte.valeurAttaque, carte.valeurDefense, carte.prixAchat, carte.prixVendre, carte.image, carte.imageDerier, 0));
-        }
-    });
+    this.http.post('api/Cartes/PostCartesUser',JSON.stringify(body), httpOptions).subscribe( response=> this.ngOnInit())
+
+  }
+  public vendre(cu:CarteUser){
+    const c=new Carte(cu.id,cu.valeurAttaque,cu.valeurDefense,cu.prixAchat,cu.prixVendre,cu.image,cu.imageDerier,cu.rezBatail);
+    const body = c;
+    const token = localStorage.getItem('Token');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      })
+    };
+    this.http.post('api/Cartes/RemoveCartesUser',JSON.stringify(body), httpOptions).subscribe( response=> this.ngOnInit())
+
   }
 
+  ngOnInit() {
+    this.carteUser.length=0;
+    this.carte.length=0;
+    const token = localStorage.getItem('Token');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      })
+    };
+    this.http.get<any>('api/Cartes/GetCartesUser', httpOptions ). subscribe( response2 => {
+      for (const carte of response2) {
+        this.carteUser.push(new CarteUser(carte.id, carte.valeurAttaque, carte.valeurDefense, carte.prixAchat, carte.prixVendre, carte.image, carte.imageDerier, 0, true));
+      }
+      console.log(this.carteUser);
+
+      this.http.get<any>('api/Cartes', httpOptions ). subscribe( response => {
+        console.log(response);
+        for (const carte of response) {
+          let carteuserbool=false;
+          for(const carteuser of this.carteUser){
+            if(carteuser.id===carte.id){
+              carteuserbool=true;
+            }
+          }
+          this.carte.push(new CarteUser(carte.id, carte.valeurAttaque, carte.valeurDefense, carte.prixAchat, carte.prixVendre, carte.image, carte.imageDerier, 0, carteuserbool));
+        }
+
+
+      });
+
+    })
+
+
+  }
+
+}
+export class CarteUser {
+  constructor(public id: number,
+              public valeurAttaque: number,
+              public valeurDefense: number,
+              public prixAchat: number,
+              public prixVendre: number,
+              public image: string,
+              public  imageDerier: string,
+              public  rezBatail: number,
+              public carteuser: boolean) {
+
+  }
 }

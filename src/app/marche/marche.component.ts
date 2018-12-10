5ec2e53e4732f27e8284c29c61c1d51cf3492490
cedfis46 <cedfis46@gmail.com>
2018-12-10 13:41:51 -0500

    marche achat vendre

--- src/app/marche/marche.component.ts
+++ src/app/marche/marche.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Carte} from '../carte/carte';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-marche',
@@ -11,8 +12,33 @@ import {Carte} from '../carte/carte';
 export class MarcheComponent implements OnInit {
   public carte: CarteUser[] = [];
   public carteUser: CarteUser[] = [];
+  public listtoutecarte: CarteUser[] = [];
 
   constructor(public router: Router, public http: HttpClient) { }
+  public listachete() {
+    this.carte = this.listtoutecarte;
+    let carte2: CarteUser[] = [];
+    for (const c of this.carte){
+      if (c.carteuser === false)
+      {
+        carte2.push(c);
+      }
+    }
+    this.carte = carte2;
+
+  }
+  public listvendre() {
+    this.carte = this.listtoutecarte;
+    let carte2: CarteUser[] = [];
+    for (const c of this.carte){
+      if (c.carteuser === true)
+      {
+        carte2.push(c);
+      }
+    }
+    this.carte = carte2;
+
+  }
 
   public achete(cu:CarteUser){
     const c=new Carte(cu.id,cu.valeurAttaque,cu.valeurDefense,cu.prixAchat,cu.prixVendre,cu.image,cu.imageDerier,cu.rezBatail);
@@ -66,9 +92,10 @@ export class MarcheComponent implements OnInit {
               carteuserbool=true;
             }
           }
+
           this.carte.push(new CarteUser(carte.id, carte.valeurAttaque, carte.valeurDefense, carte.prixAchat, carte.prixVendre, carte.image, carte.imageDerier, 0, carteuserbool));
         }
-
+        this.listtoutecarte = this.carte;
 
       });
 
@@ -79,15 +106,15 @@ export class MarcheComponent implements OnInit {
 
 }
 export class CarteUser {
-  constructor(public id: number,
-              public valeurAttaque: number,
-              public valeurDefense: number,
-              public prixAchat: number,
-              public prixVendre: number,
-              public image: string,
-              public  imageDerier: string,
-              public  rezBatail: number,
-              public carteuser: boolean) {
+    constructor(public id: number,
+      public valeurAttaque: number,
+      public valeurDefense: number,
+      public prixAchat: number,
+      public prixVendre: number,
+      public image: string,
+      public  imageDerier: string,
+      public  rezBatail: number,
+      public carteuser: boolean) {
 
   }
 }

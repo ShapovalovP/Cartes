2ff791e8d7f7e06a146980ab7567a13c6daae46d
Kademor <1555108@LLBD0605INFO13>
2018-12-18 14:17:02 -0500

    Liste deck marche

--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Router} from '@angular/router';
 import {Carte} from '../carte/carte';
 import {Deck} from './Deck';
@@ -14,7 +14,15 @@ export class DeckComponent implements OnInit {
   constructor(public router: Router, public http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<any>('api/Decks'). subscribe( response => {
+    const token = localStorage.getItem('Token');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      })
+    };
+
+    this.http.get<any>('api/Decks', httpOptions). subscribe( response => {
       console.log(response);
       for (const dbDecks of response) {
         this.decks.push(new Deck(dbDecks.Id, dbDecks.name));

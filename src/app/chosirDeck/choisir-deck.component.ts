76a783fa188f0618efe48ef1ab20faf243d24b48
ShapovalovP <44822502+ShapovalovP@users.noreply.github.com>
2018-12-20 19:24:28 -0500

    fix choisir-deck

--- src/app/chosirDeck/choisir-deck.component.ts
+++ src/app/chosirDeck/choisir-deck.component.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Deck} from '../deck/Deck';
 import {Carte} from '../carte/carte';
+import {CartEnemi} from './cart-enemi';
 
 @Component({
   selector: 'app-choisir-deck',
@@ -27,7 +28,11 @@ export class ChoisirDeckComponent implements OnInit {
   public  point: number = 0;
   public  usersPoint: string ;
 
+  messageChat = '';
 
+  public  socketChat: WebSocket;
+
+  public mess: CartEnemi = null ;
 
   ////////////////
   public deckPret: boolean = false;
@@ -51,6 +56,14 @@ export class ChoisirDeckComponent implements OnInit {
     if ( this.deckPret === false) {
       this.listDeckInical ();
     }
+    this.socketChat = new WebSocket('ws://localhost:8180');
+    this.socketChat.onopen = () => {
+      this.socketChat.send(JSON.stringify( { ConnectingUserName: 'a'} ));
+    }
+    this.socketChat.onmessage = (message) =>  {
+      console.log(message.data);
+      this.parsCartEnemi(message.data);
+    };
   }
   listDeckInical () {
     const token = localStorage.getItem('Token');
@@ -74,6 +87,58 @@ export class ChoisirDeckComponent implements OnInit {
         console.log(this.listcarte);
       }
     });
+  }
+
+  selectFromDeck( cart: Carte ) {
+    if (this.point === 0 && this.votreTour === true) {
+      this.suprimCartTab(cart, this.tabCartUser);
+      const kart: Carte = new Carte( this.tabCartUser.length + 5, cart.valeurAttaque, cart.valeurDefense,
+        cart.prixAchat, cart.prixVendre, cart.image, cart.imageDerier, cart.rezBatail );
+      this.tabCartUserParti.push(kart);
+      this.envoierCart(cart);
+      if (this.tabCartComputerParti.length === 0) { ///////// zdes computer podymaet nado li dobavliat carty i kakyu
+        // this.getComputersCart();
+      }
+      this.votreTour = false;
+    }
+  }
+
+  ngOnDestroy() {
+    this.socketChat.close();
+  }
+
+
+  parsCartEnemi (re: any): CartEnemi {
+    let vu: CartEnemi;
+    vu = new CartEnemi(re.cartId, re.utilisateurUserName);
+    this.mess = vu;
+
+    return vu;
+  }
+  envoierCart (cart: Carte) {
+    const token = localStorage.getItem('Token');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      })
+    };
+    return this.http.post('api/Cartes/AddCarteTour', cart , httpOptions).subscribe(r => {
+      // this.selectFromDeck(cart);
+    } );
+  }
+  sendMessageChat() {
+    this.socketChat.send(this.messageChat);
+    this.messageChat = '';
+  }
+
+  suprimCartTab (cart: Carte, tab: Carte[]) {
+    for (let i = 0; i < tab.length; i++) {
+      if (tab[i].id === cart.id) {
+        tab.splice(i, 1);
+        return;
+      }
+    }
   }
 
 }
